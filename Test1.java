b9c457a97daca06d3a6069ad388988af4e1ff94e
Formal codes goto: tomxue <tomxue@outlook.com>
2013-09-07 12:03:33 +0800

    Java print numbers from 0 to 100

---
+++ Test1.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+public class Test1 {
+    public static void main(String[] args) throws IOException {
+        int i;
+        for(i=0;i<1000;i++)
+            System.out.println(i);
+    }
+}
